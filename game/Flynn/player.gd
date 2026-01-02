2f019c7a44e8c43e6e50e995c2f785ae5e9af935
Adam Steven <93397904+SebbyMortimer@users.noreply.github.com>
2026-01-02 00:52:03 +0000

    Day 1 commit with initial game and journal

---
+++ game/Flynn/player.gd
@@ -0,0 +1,51 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+@onready var cameraRotatePoint = $CameraRotatePoint
+
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _process(delta: float) -> void:
+	cameraRotatePoint.position = position
+	# manually update position as I enabled top level to disconnect from the players rotation
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction = Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, cameraRotatePoint.rotation.y).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+		
+		#rotation.y = lerp_angle(rotation.y, cameraRotatePoint.rotation.y, 0.1)
+		rotation.y = lerp_angle(rotation.y, atan2(-velocity.x, -velocity.z), 0.1)
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	move_and_slide()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		var mouse_delta = event.relative
+		
+		var camRotation = cameraRotatePoint.rotation_degrees - Vector3(mouse_delta.y / 10, mouse_delta.x / 10, 0)
+		camRotation.x = clampf(camRotation.x, -80, 80)
+		cameraRotatePoint.rotation_degrees = camRotation
---
+++ journal.md
@@ -0,0 +1,7 @@
+# Console Game
+
+## Week 1: Making the game
+
+### Day 1
+
+Today I worked on importing the Flynn character model from Skylanders Giants, getting him cleaned up and setting him up inside the game. To set him up, I began programming the player and camera movement, the player can move in all 4 directions and should be pretty easy to swap over to joystick controls once I've created the controller. The player smoothly rotates in the direction that they're walking and the camera can orbit around the player's body. However, it currently orbits their feet so tomorrow I'll try to move it up so that the camera orbits around the player's head instead. I also quickly added jumping but I want to tweak the speed at which they fall back down because right now the player feels quite floaty.
